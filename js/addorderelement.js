152734fa9a97bfbd5d1f2d0f77afe6c0f68f2135
Ákos Mészáros <winnerduo@gmail.com>
2023-02-17 14:35:56 +0100

    addorderelements.js átírva, hogy új html elemeket hozzon létre

--- js/addorderelement.js
+++ js/addorderelement.js
@@ -2,39 +2,43 @@ var orderValue = 0.0;
 
 function addOrderItem(pizzaType, pizzaCost) {
     document.getElementById("order-button").removeAttribute("disabled");
+
+    var newElement = document.createElement("label");
     switch (pizzaType) {
         case "margherita":
-            document.getElementById("current-order").value += "- Margherita\n";
+            newElement.innerHTML = "<b>- Margherita</b><br>";
             break;
         case "pepperoni":
-            document.getElementById("current-order").value += "- Pepperoni\n";
+            newElement.innerHTML = "<b>- Pepperoni</b><br>";
             break;
         case "hawaiian":
-            document.getElementById("current-order").value += "- Hawaiian\n";
+            newElement.innerHTML = "<b>- Hawaiian</b><br>";
             break;
         case "meatlovers":
-            document.getElementById("current-order").value += "- Meat Lover's\n";
+            newElement.innerHTML = "<b>- Meat Lover's</b><br>";
             break;
         case "vegetarian":
-            document.getElementById("current-order").value += "- Vegetarian\n";
+            newElement.innerHTML = "<b>- Vegetarian</b><br>";
             break;
         case "bbqchicken":
-            document.getElementById("current-order").value += "- BBQ Chicken\n";
+            newElement.innerHTML = "<b>- BBQ Chicken</b><br>";
             break;
         case "buffalochicken":
-            document.getElementById("current-order").value += "- Buffalo Chicken\n";
+            newElement.innerHTML = "<b>- Buffalo Chicken</b><br>";
             break;
         case "whitepizza":
-            document.getElementById("current-order").value += "- White Pizza\n";
+            newElement.innerHTML = "<b>- White Pizza</b><br>";
             break;
         case "meatball":
-            document.getElementById("current-order").value += "- Meatball\n";
+            newElement.innerHTML = "<b>- Meatball</b><br>";
             break;
         case "supreme":
-            document.getElementById("current-order").value += "- Supreme\n";
+            newElement.innerHTML = "<b>- Supreme</b><br>";
             break;
     }
-    document.getElementById("current-order").value += "$" + pizzaCost.toFixed(2) + "\n";
+    newElement.innerHTML += "$" + pizzaCost.toFixed(2) + "<br>";
+
+    document.getElementById("current-order").appendChild(newElement);
     orderValue += pizzaCost;
     document.getElementById("order-price").value = orderValue;
     document.getElementById("price-display").textContent = "Total: $" + orderValue.toFixed(2);
